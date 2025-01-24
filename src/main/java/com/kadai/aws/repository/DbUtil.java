e54ae9113ab63de80512b7ab28f37d300f7c9fcd
sakuraM25220266 <sakura1218m@gmail.com>
2025-01-24 17:25:29 +0900

    プロパティファイル読み込みのメソッドを作成した

--- src/main/java/com/kadai/aws/repository/DbUtil.java
+++ src/main/java/com/kadai/aws/repository/DbUtil.java
@@ -1,8 +1,16 @@
 package com.kadai.aws.repository;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class DbUtil {
 
@@ -10,10 +18,18 @@ public class DbUtil {
 		ON, OFF,
 		;
 	}
-	
-	private static final String URL = "jdbc:hsqldb:hsql://localhost/";
-    private static final String USERNAME = "sa";
-    private static final String PASSWORD = "";
+
+	private static final Logger logger = LogManager.getLogger(DbUtil.class);
+
+	private static Properties properties = new Properties();
+
+	public static final String DEFAULT_SETTING_PROPERTIES_FILE = "/WEB-INF/classes/application.properties";
+
+	private static boolean isInit = false;
+
+	private static String URL;
+	private static String USERNAME;
+	private static String PASSWORD;
 
 	/**
 	 * オートコミットモードをオフにしてコネクションを取り出す。
@@ -26,15 +42,9 @@ public class DbUtil {
 	}
 
 	public static Connection getConnection(AutoCommitMode autoCommitMode) throws SQLException {
-		// HSQLDB JDBCドライバの手動ロード
-	    try {
-	        Class.forName("org.hsqldb.jdbc.JDBCDriver");
-	    } catch (ClassNotFoundException e) {
-	        e.printStackTrace();
-	        throw new SQLException("HSQLDB JDBC driver not found", e);
-	    }
-	    
-	    //コネクションの取得
+		init();
+
+		//コネクションの取得
 		Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 		if (autoCommitMode == AutoCommitMode.ON) {
 			conn.setAutoCommit(true);
@@ -64,5 +74,42 @@ public class DbUtil {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	/**
+	 * 
+	 * @param filename
+	 * @throws SQLException
+	 */
+	protected static void init(String filename) throws SQLException {
+		try (InputStream in = new FileInputStream(new File(filename))) {
+			properties.load(in);
+		} catch (IOException e) {
+			logger.error("初期化に失敗しました。[filename={}]", filename, e);
+			throw new SQLException("初期化に失敗しました。", e);
+		}
+		loadDriver();
+		URL = properties.getProperty("spring.datasource.url");
+		USERNAME = properties.getProperty("spring.datasource.username");
+		PASSWORD = properties.getProperty("spring.datasource.password");
+		isInit = true;
+
+	}
+
+	private static void loadDriver() throws SQLException {
+		// HSQLDB JDBCドライバの手動ロード
+		try {
+			Class.forName(properties.getProperty("spring.datasource.driverClassName"));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();//loggerになおす
+			throw new SQLException("JDBCドライバのロードに失敗しました", e);
+		}
+	}
+
+	protected static void init() throws SQLException {
+		if (isInit) {
+			return;
+		}
+		init(DEFAULT_SETTING_PROPERTIES_FILE);
 	}
 }
